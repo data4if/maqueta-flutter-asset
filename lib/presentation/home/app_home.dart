e98365866ee34b09ea388b10ab2583da5697d165
carlos4if <carloslopezdata4if@gmail.com>
2024-07-29 21:02:55 -0400

    inicio de estructura de la pagina principal y sus widgets

--- lib/presentation/home/app_home.dart
+++ lib/presentation/home/app_home.dart
@@ -34,6 +34,7 @@ class AppHome extends StatelessWidget {
                   InkWell(
                     child: SizedBox(
                       height: 0.0549 * (width),
+                      width: 0.0549 * (width),
                       child: Center(
                         child: Text(
                           '¿Quiénes somos?',
@@ -52,6 +53,7 @@ class AppHome extends StatelessWidget {
                   ),
                   SizedBox(
                     height: 0.0549 * (width),
+                    width: 0.0549 * (width),
                     child: Center(
                       child: IconButton(
                         onPressed: () {
@@ -89,71 +91,5 @@ class AppHome extends StatelessWidget {
         );
       },
     );
-  }
-}
-
-class MainPage extends StatefulWidget {
-  const MainPage({super.key});
-  @override
-  _MainPageState createState() => _MainPageState();
-}
-
-class _MainPageState extends State<MainPage> {
-  int _currentIndex = 0;
-  final List<String> _imageList = [
-    'assets/images/main/main_1.jpeg',
-    'assets/images/main/main_2.jpeg',
-    'assets/images/main/main_3.jpeg',
-  ];
-  void startTimer() {
-    Future.delayed(const Duration(seconds: 5), () {
-      if (mounted) {
-        setState(() {
-          _currentIndex = (_currentIndex + 1) % _imageList.length;
-        });
-      }
-      startTimer();
-    });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    startTimer();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return LayoutBuilder(
-        builder: (BuildContext context, BoxConstraints constraints) {
-      double width = constraints.maxWidth;
-      return Stack(
-        alignment: Alignment.center,
-        children: [
-          AnimatedSwitcher(
-            duration: const Duration(milliseconds: 500),
-            child: Image.asset(
-              _imageList[_currentIndex],
-              height: 0.4907 * (width),
-              width: width,
-              fit: BoxFit.cover,
-              key: ValueKey(_imageList[_currentIndex]),
-            ),
-            transitionBuilder: (Widget child, Animation<double> animation) {
-              return FadeTransition(
-                opacity: animation,
-                child: child,
-              );
-            },
-          ),
-          Image.asset(
-            width: 0.6766 * (width),
-            height: 0.2771 * (width),
-            "images/brand/conoce.png", // Corregida la ruta de la imagen
-            //fit: BoxFit.cover,
-          )
-        ],
-      );
-    });
   }
 }
