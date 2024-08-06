92f8aeddd004dd79f6b142504e58d1f39102bfe0
carlos4if <carloslopezdata4if@gmail.com>
2024-08-06 11:03:27 -0400

    fixed error url invalida

--- lib/presentation/home/widgets/event_card.dart
+++ lib/presentation/home/widgets/event_card.dart
@@ -1,18 +1,29 @@
 import 'package:conoce_gama/configuration/core/app_theme.dart';
 import 'package:conoce_gama/domain/event.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EventWidget extends StatelessWidget {
   final Event event;
   final double width;
+
   EventWidget({super.key, required this.event, required this.width});
+
+  Future<void> _launchUrl(String url) async {
+    final Uri _url = Uri.parse(url);
+    try {
+      if (!await launchUrl(_url)) {
+        throw Exception('Could not launch $_url');
+      }
+    } on Exception catch (e) {
+      print(e.toString());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
       builder: (BuildContext context, BoxConstraints constraints) {
-        //var width = constraints.maxWidth;
         return Row(
           children: [
             SizedBox(
@@ -21,42 +32,45 @@ class EventWidget extends StatelessWidget {
             Container(
               width: (406 / 1512) * width,
               height: (512 / 1512) * width,
-              decoration: const BoxDecoration(color: Colors.white, boxShadow: [
-                BoxShadow(
-                  color: Colors.black54, // Shadow color
-                  blurRadius: 0.5, // Shadow blur radius
-                  spreadRadius: 0.5, // Shadow spread radius
-                  offset: Offset(0, 2), // Changes the position of the shadow
-                ),
-              ]),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black54,
+                    blurRadius: 0.5,
+                    spreadRadius: 0.5,
+                    offset: Offset(0, 2),
+                  ),
+                ],
+              ),
               child: Center(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      child: Stack(
-                        alignment: Alignment.bottomCenter,
-                        children: [
-                          Image.asset(
-                            event.imageUrl,
-                            width: (406 / 1512) * width,
-                            height: (315 / 1512) * width,
+                    Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Image.asset(
+                          event.imageUrl,
+                          width: (406 / 1512) * width,
+                          height: (315 / 1512) * width,
+                        ),
+                        Container(
+                          width: (406 / 1512) * width,
+                          height: (19 / 1512) * width,
+                          decoration: const BoxDecoration(
+                            color: AppTheme.greentColorOpacity,
                           ),
-                          Container(
-                              width: (406 / 1512) * width,
-                              height: (19 / 1512) * width,
-                              decoration: const BoxDecoration(
-                                color: AppTheme.greentColorOpacity,
-                              )),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                     Container(
-                        width: (406 / 1512) * width,
-                        height: (19 / 1512) * width,
-                        decoration: const BoxDecoration(
-                          color: AppTheme.greentColorNoOpacity,
-                        )),
+                      width: (406 / 1512) * width,
+                      height: (19 / 1512) * width,
+                      decoration: const BoxDecoration(
+                        color: AppTheme.greentColorNoOpacity,
+                      ),
+                    ),
                     SizedBox(
                       height: (70 / 1512) * width,
                       child: Padding(
@@ -78,36 +92,30 @@ class EventWidget extends StatelessWidget {
                       mainAxisSize: MainAxisSize.min,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        SizedBox(
-                          //height: (45 / 1512) * width,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 5),
-                            child: Text(
-                              'Fecha: ' + event.date,
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontFamily: 'alcaldia_fonts',
-                                color: Colors.black,
-                                height: 0.9,
-                                fontSize: (20 / 1512) * width,
-                                fontWeight: FontWeight.normal,
-                              ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Text(
+                            'Fecha: ' + event.date,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'alcaldia_fonts',
+                              color: Colors.black,
+                              height: 0.9,
+                              fontSize: (20 / 1512) * width,
+                              fontWeight: FontWeight.normal,
                             ),
                           ),
                         ),
-                        Container(
-                          //height: (45 / 1512) * width,
-                          child: Padding(
-                            padding: const EdgeInsets.only(left: 5),
-                            child: Text(
-                              'Lugar: ' + event.place,
-                              textAlign: TextAlign.left,
-                              style: TextStyle(
-                                fontFamily: 'alcaldia_fonts',
-                                color: Colors.black,
-                                fontSize: (20 / 1512) * width,
-                                fontWeight: FontWeight.normal,
-                              ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 5),
+                          child: Text(
+                            'Lugar: ' + event.place,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              fontFamily: 'alcaldia_fonts',
+                              color: Colors.black,
+                              fontSize: (20 / 1512) * width,
+                              fontWeight: FontWeight.normal,
                             ),
                           ),
                         ),
@@ -117,32 +125,23 @@ class EventWidget extends StatelessWidget {
                       children: [
                         InkWell(
                           onTap: () {
-                            openURL(event.link);
+                            _launchUrl(event.link);
                           },
                           child: Padding(
-                            padding: const EdgeInsets.only(
-                              left: 5,
-                              top: 10,
-                            ),
+                            padding: const EdgeInsets.only(left: 5),
                             child: Text(
                               'Link red social',
                               style: TextStyle(
-                                fontSize: (20 / 1512) * width,
-                                fontFamily: 'alcaldia_font',
-                                color: AppTheme.greentColorOpacity,
+                                fontSize: (18 / 1512) * width,
+                                fontFamily: 'alcaldia_fonts',
+                                color: AppTheme.primaryColor,
                                 decoration: TextDecoration.underline,
                               ),
                             ),
                           ),
                         ),
-                        /* Image.asset(
-                          "images/brand/sun_spiral.png",
-                          width: (5 / 1512) * width,
-                          height: (5 / 1512) * width,
-                          //fit: BoxFit.cover,
-                        ) */
                       ],
-                    )
+                    ),
                   ],
                 ),
               ),
@@ -154,9 +153,5 @@ class EventWidget extends StatelessWidget {
         );
       },
     );
-  }
-
-  void openURL(String url) {
-    SystemChannels.platform.invokeMethod('SystemNavigator.pop', url);
   }
 }
